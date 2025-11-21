1cc9875be5931c7d2fa1ae729162f219993e7814
yazarahmet <yazarahmet@gmail.com>
2025-11-21 22:45:24 +0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -18,6 +18,12 @@ export const PlayIcon = ({ className }: { className?: string }) => (
   </svg>
 );
 
+export const PauseIcon = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+  </svg>
+);
+
 export const SendIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
